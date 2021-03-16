ad804117ccdc4cb3442ea207e27451a77606aaa1
Martin <hazi.martin@gmail.com>
2021-03-16 09:56:48 +0100

    Added basic member variables and methods to the Game and Point classes

--- ConsoleSnake/Game.cs
+++ ConsoleSnake/Game.cs
@@ -8,6 +8,36 @@ namespace ConsoleSnake
     // Manages game loop, snake, obstacles
     class Game
     {
+        private bool inGame; // Stores the current state of the game, true if the game is on
+        private Snake snake; // Represents the snake that can be contrelled by the player
+        List<Obstacle> obstalces; // Stores all of the obstacles on the map
+        private int speed; // stores the speed of the game in milliseconds, the time interval to refresh the screen
 
+        public Game()
+        {
+            inGame = false;
+            snake = new Snake();
+            obstalces = new List<Obstacle>();
+        }
+
+        // Can be called from outside of the class to start the game
+        public void StartGame()
+        {
+            inGame = true;
+            PlayGame();
+        }
+
+        // Plays the game until a the snake hits itself, an obstacle or the edge of the map
+        // Reads input from Console, then change behavior based on the input
+        private void PlayGame()
+        {
+
+        }
+
+        // Checks snake collision with the edge of the map, obstacles and istself
+        private bool CheckImpact()
+        {
+            return false;
+        }
     }
 }
--- ConsoleSnake/Point.cs
+++ ConsoleSnake/Point.cs
@@ -7,5 +7,33 @@ namespace ConsoleSnake
     // Represent a point on the map with x and y coordinates
     class Point
     {
+        public int X { get; set; } // the x coordinate of the point
+        public int Y { get; set; } // the y coordinate of the point
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        
+        public Point(Point point)
+        {
+            this.X = point.X;
+            this.Y = point.Y;
+        }
+
+        // Sets the x and y coordinates of the point to the given parameters
+        public void SetPosition(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        // Sets the x and y coordinates of the point to the coordinates of the given point
+        public void SetPosition(Point point)
+        {
+            this.X = point.X;
+            this.Y = point.Y;
+        }
     }
 }
